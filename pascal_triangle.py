bfa5930ab5fbc2b4a7607f2d19079fe5e9fe15c9
Abhishek <abhiswc29@gmail.com>
2022-01-26 18:50:49 +0530

    second commit

---
+++ pascal_triangle.py
@@ -0,0 +1,12 @@
+def pascal(rows):
+    res = [[1]]
+    for i in range(rows-1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1])+1):
+            row.append(temp[j]+temp[j+1])
+        res.append(row)
+    return res
+
+
+print(pascal(5))
